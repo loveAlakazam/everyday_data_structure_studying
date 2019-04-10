55dfcfcd3ffc23036e712add556f88b4f935baa4
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-04-11 01:28:17 +0900

    Create frogOneRiver_54_3.py

---
+++ 0411_2019/frogOneRiver_54_3.py
@@ -0,0 +1,14 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, A):
+    # write your code in Python 3.6
+    elements=list(range(1,X+1))
+    idxs=[]
+    for e in elements:
+        if (e in A) is False:
+            return -1
+        
+        idxs.append(A.index(e))
+    return max(idxs)
+    
