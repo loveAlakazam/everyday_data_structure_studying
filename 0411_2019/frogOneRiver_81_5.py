b360a1650ccd0f03a559aee99c4a35616cb3c251
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-04-11 02:10:46 +0900

    Update frogOneRiver_81_5.py

--- 0411_2019/frogOneRiver_81_5.py
+++ 0411_2019/frogOneRiver_81_5.py
@@ -1,6 +1,7 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
-
+# (url) 
+# https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/
 def solution(X, A):
     # write your code in Python 3.6
     path=X*[0]
