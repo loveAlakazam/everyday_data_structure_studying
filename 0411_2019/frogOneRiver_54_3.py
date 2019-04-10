bc92eb3c66f477a93a4d17038a604b767b99cc46
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2019-04-11 01:54:26 +0900

    Update frogOneRiver_54_3.py

--- 0411_2019/frogOneRiver_54_3.py
+++ 0411_2019/frogOneRiver_54_3.py
@@ -11,4 +11,18 @@ def solution(X, A):
         
         idxs.append(A.index(e))
     return max(idxs)
-    
+
+#
+# trial 4 -> result=54
+'''
+def solution(X, A):
+    # write your code in Python 3.6
+    elements=list(range(1,X+1))
+    idxs=[]
+    for e in elements:
+        if (e in A) is False:
+            return -1
+        
+        idxs.append(A.index(e))
+    return max(idxs)
+'''
